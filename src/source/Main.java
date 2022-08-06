ec66ec1d7d358f2d14f20756d836e14c36c47060
SteezyKyd <steezykydweb@gmail.com>
2022-08-06 13:45:02 +0300

    Hello world!

--- src/source/Main.java
+++ src/source/Main.java
@@ -3,7 +3,7 @@ package source;
 public class Main {
     public static void main(String[] args) {
     int a,b,c;
-        /*System.out.println("Hello world!");*/
+        System.out.println("Hello world!");
 
         /*byte numm = 100; //8 бит => -128 - 127
         short numm1 = 131; //8 бит => -32768 - 32767*/
