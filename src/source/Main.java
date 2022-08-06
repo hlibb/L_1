5ac0f6ea5c9b2e66d8d2ca356efd022f1a80e1ca
SteezyKyd <steezykydweb@gmail.com>
2022-08-06 13:43:02 +0300

    Initial commit

---
+++ src/source/Main.java
@@ -0,0 +1,17 @@
+package source;
+
+public class Main {
+    public static void main(String[] args) {
+    int a,b,c;
+        /*System.out.println("Hello world!");*/
+
+        /*byte numm = 100; //8 бит => -128 - 127
+        short numm1 = 131; //8 бит => -32768 - 32767*/
+        int FirstNumber = 8912;
+        int SecondNumber = 4;
+
+        int Result = SecondNumber * FirstNumber;
+        System.out.println(Result);
+
+    }
+}
